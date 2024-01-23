c6171a1844ba07170a20030a5e5384dc59c584e6
Himanshu Bisht <trickyonline672@gmail.com>
2024-01-23 11:07:38 +0530

    initial commit

---
+++ lib/constants/strings.dart
@@ -0,0 +1,4 @@
+class Mycredentials {
+  static const String clientID = "1b38a4bdaa0a4f22b22ac357fce5046c";
+  static const String clientSecret = "9de9a684c65b43c8bbe63fb9108c7a0b";
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import file_selector_macos
+import flutter_web_auth
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
