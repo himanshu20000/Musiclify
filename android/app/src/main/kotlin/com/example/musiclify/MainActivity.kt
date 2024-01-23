153a994baf6fe54955ff0bceef222f34e4b9a6f5
Himanshu Bisht <trickyonline672@gmail.com>
2024-01-23 12:27:25 +0530

    Add string.dart to .gitignore

--- android/app/src/main/kotlin/com/example/musiclify/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.musiclify
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/LyricsModel.dart
+++
@@ -1,6 +0,0 @@
-class LyricsModel {
-  final String words;
-  final DateTime timeStamp;
-
-  LyricsModel(this.words, this.timeStamp);
-}
